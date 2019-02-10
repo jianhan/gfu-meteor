33637ba01bf5c7599c59a427e988020708ecccd6
Jian Han <jianhan@mail.com>
2019-02-10 15:12:53 +1000

    [feat] added atlaskit css

--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
+import "../node_modules/@atlaskit/css-reset/dist/bundle.css";
 
 import App from "../imports/ui/layouts/App";
 
--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
 import AccountsUIWrapper from "../components/AccountsUIWrapper";
+import Badge from "@atlaskit/badge";
+import Page, { Grid, GridColumn } from "@atlaskit/page";
 
 // App component - represents the whole app
 export default class App extends Component {
   render() {
     return (
-      <div className="container">
-        <AccountsUIWrapper />
-      </div>
+      <Page>
+        <Grid>
+          <GridColumn medium={8}>
+            <h1>Main heading</h1>
+            <p>
+              Lorem ipsum dolor sit amet and consectetur adipisicing elit.
+              Blanditiis voluptatum perspiciatis doloribus dignissimos accusamus
+              <Badge>{1000}</Badge>
+              <AccountsUIWrapper />
+            </p>
+          </GridColumn>
+          <GridColumn medium={4}>
+            <h2>Sidebar</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis voluptatum perspiciatis doloribus dignissimos accusamus
+              commodi, nobis ut, error iusto, quas vitae nesciunt consequatur
+              possimus labore! Mollitia est quis minima asperiores.
+            </p>
+          </GridColumn>
+          <GridColumn>
+            <h2>Content below which takes up remaining space</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis voluptatum perspiciatis doloribus dignissimos accusamus
+              commodi, nobis ut, error iusto, quas vitae nesciunt consequatur
+              possimus labore! Mollitia est quis minima asperiores.
+            </p>
+          </GridColumn>
+        </Grid>
+      </Page>
     );
   }
 }
