3738c0a38f578f302159c7c67730d82fbeadfcf3
Jian Han <jianhan@mail.com>
2019-02-12 23:44:03 +1000

    [feat] setup navigation

--- imports/ui/layouts/Main.jsx
+++ imports/ui/layouts/Main.jsx
@@ -2,11 +2,89 @@ import React, { Component } from "react";
 import AccountsUIWrapper from "../components/AccountsUIWrapper";
 import { Link } from "react-router-dom";
 import BlazeTemplate from "../components/BlazeTemplate";
-import { Button } from "react-bootstrap";
+import {
+  Button,
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl
+} from "react-bootstrap";
 
 // App component - represents the whole app
 export default class Main extends Component {
   render() {
-    return <Button variant="primary">Primary</Button>;
+    return (
+      <Container>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+        <Row>
+          <Col md={12} />
+        </Row>
+        {/* Stack the columns on mobile by making one full-width and the other half-width */}
+        <Row>
+          <Col xs={12} md={8}>
+            xs=12 md=8
+            <BlazeTemplate name="atForm" />
+            <AccountsUIWrapper />
+            <Button variant="primary">Primary</Button>
+          </Col>
+          <Col xs={6} md={4}>
+            xl=6 md=4
+          </Col>
+        </Row>
+
+        {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+        <Row>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+        </Row>
+
+        {/* Columns are always 50% wide, on mobile and desktop */}
+        <Row>
+          <Col xs={6}>xs=6</Col>
+          <Col xs={6}>xs=6</Col>
+        </Row>
+      </Container>
+    );
   }
 }
